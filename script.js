a10bead318b9bfee91575ae3865cc9f2c16d0ad1
Md Kaif Manzar <kaifmanzar321@gmail.com>
2023-11-30 11:45:35 +0530

    gsap work

--- script.js
+++ script.js
@@ -5,7 +5,26 @@
 //     smooth: true
 // });
 
-
+gsap.from("#about .about_content .image_section", {
+    x: -500,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger: {
+        trigger: "#about .about_content .image_section",
+        scroller: "body",
+    },
+    delay: 0.5, 
+});
+gsap.from("#about .about_content .text_section", {
+    y: 500,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger: {
+        trigger: "#about .about_content .image_section",
+        scroller: "body",
+    },
+    delay: 0.5, 
+});
 
 var flag=true;
 
